e3486b1b32562096ef74e4a07339f57c37a70df1
gewoongianni <54664140+GewoonGianni@users.noreply.github.com>
2020-11-12 13:17:07 +0100

    did the intake

---
+++ intake/src/com/giannissupervettesoftwarebedrijfbvllcfirma/Main.java
@@ -0,0 +1,8 @@
+package com.giannissupervettesoftwarebedrijfbvllcfirma;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Can we get a big W in the console");
+    }
+}
