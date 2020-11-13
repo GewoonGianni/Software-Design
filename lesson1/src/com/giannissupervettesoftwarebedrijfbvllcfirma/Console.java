2dea604fa78a6a353280dcea4be26fad8cc7abb8
gewoongianni <54664140+GewoonGianni@users.noreply.github.com>
2020-11-13 15:40:43 +0100

    wizzardry from loek

--- lesson1/src/com/giannissupervettesoftwarebedrijfbvllcfirma/Console.java
+++ lesson1/src/com/giannissupervettesoftwarebedrijfbvllcfirma/Console.java
@@ -1,6 +1,15 @@
 package com.giannissupervettesoftwarebedrijfbvllcfirma;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Console {
+
+    public Console() throws FileNotFoundException {
+        System.setOut(new PrintStream(new File("studenten.txt")));
+    }
+
     static void print(String message){
         System.out.println(message);
     }
