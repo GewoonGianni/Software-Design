8165794dd80b56173421cb77c609037751badc90
gewoongianni <54664140+GewoonGianni@users.noreply.github.com>
2020-11-18 16:44:51 +0100

    add refactoring 1

---
+++ refactorings1_rename/javalin/src/main/java/io/javalin/core/PathParser.kt
@@ -0,0 +1,56 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin.core
+
+import io.javalin.http.util.ContextUtil
+
+class PathParser(path: String, ignoreTrailingSlashes: Boolean) {
+
+    val segments: List<PathSegment> = path.split("/")
+            .filter { it.isNotEmpty() }
+            .map {
+                when {
+                    it.startsWith(":") -> PathSegment.Parameter(it.removePrefix(":"))
+                    it == "*" -> PathSegment.Wildcard
+                    else -> PathSegment.Normal(it)
+                }
+            }
+
+    val pathParamNames = segments.filterIsInstance<PathSegment.Parameter>().map { it.name }
+
+    //compute matchRegex suffix : if ignoreTrailingSlashes config is set we keep /?, else we use the true path trailing slash : present or absent
+    private val matchRegexSuffix = if (ignoreTrailingSlashes) "/?" else if (path.endsWith("/")) "/" else ""
+    private val matchRegex = ("^/${segments.joinToString("/") { it.asRegexString() }}" + matchRegexSuffix + "$").toRegex()
+
+    private val pathParamRegex = matchRegex.pattern.replace("[^/]+?", "([^/]+?)").toRegex()
+
+    fun matches(url: String): Boolean = url matches matchRegex
+
+    fun extractPathParams(url: String) = pathParamNames.zip(values(pathParamRegex, url)) { name, value ->
+        name to ContextUtil.urlDecode(value)
+    }.toMap()
+
+    // Match and group values, then drop first element (the input string)
+    private fun values(regex: Regex, url: String) = regex.matchEntire(url)?.groupValues?.drop(1) ?: emptyList()
+}
+
+sealed class PathSegment {
+
+    internal abstract fun asRegexString(): String
+
+    class Normal(val content: String) : PathSegment() {
+        override fun asRegexString(): String = content
+    }
+
+    class Parameter(val name: String) : PathSegment() {
+        override fun asRegexString(): String = "[^/]+?" // Accepting everything except slash
+    }
+
+    object Wildcard : PathSegment() {
+        override fun asRegexString(): String = ".*?" // Accept everything
+    }
+}
---
+++ refactorings1_rename/javalin/src/main/java/io/javalin/http/staticfiles/JettyResourceHandler.kt
@@ -0,0 +1,120 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin.http.staticfiles
+
+import io.javalin.Javalin
+import io.javalin.core.util.Header
+import io.javalin.core.util.Util
+import io.javalin.http.JavalinResponseWrapper
+import org.eclipse.jetty.server.Request
+import org.eclipse.jetty.server.handler.ContextHandler.AliasCheck
+import org.eclipse.jetty.server.handler.ResourceHandler
+import org.eclipse.jetty.util.URIUtil
+import org.eclipse.jetty.util.resource.EmptyResource
+import org.eclipse.jetty.util.resource.Resource
+import java.io.File
+import java.nio.file.AccessDeniedException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import io.javalin.http.staticfiles.ResourceHandler as JavalinResourceHandler
+
+class JettyResourceHandler(val precompressStaticFiles: Boolean = false, private val aliasCheck: AliasCheck? = null) : JavalinResourceHandler {
+
+    val handlers = mutableListOf<ResourceHandler>()
+
+    override fun addStaticFileConfig(config: StaticFileConfig) {
+        handlers.add(when {
+            config.path == "/webjars" -> WebjarHandler()
+            aliasCheck != null -> AliasHandler(config, aliasCheck)
+            else -> PrefixableHandler(config)
+        }.apply { start() })
+    }
+
+    override fun handle(httpRequest: HttpServletRequest, httpResponse: HttpServletResponse): Boolean {
+        val target = httpRequest.getAttribute("jetty-target") as String
+        val baseRequest = httpRequest.getAttribute("jetty-request") as Request
+        for (handler in handlers) {
+            try {
+                val resource = handler.getResource(target)
+                if (resource.isFile() || resource.isDirectoryWithWelcomeFile(handler, target)) {
+                    val maxAge = if (target.startsWith("/immutable/") || handler is WebjarHandler) 31622400 else 0
+                    httpResponse.setHeader(Header.CACHE_CONTROL, "max-age=$maxAge")
+                    // Remove the default content type because Jetty will not set the correct one
+                    // if the HTTP response already has a content type set
+                    if (precompressStaticFiles && PrecompressingResourceHandler.handle(resource, httpRequest, httpResponse)) {
+                        return true
+                    }
+                    httpResponse.contentType = null
+                    handler.handle(target, baseRequest, httpRequest, httpResponse)
+                    httpRequest.setAttribute("handled-as-static-file", true)
+                    (httpResponse as JavalinResponseWrapper).outputStream.finalize()
+                    return true
+                }
+            } catch (e: Exception) { // it's fine, we'll just 404
+                if (!Util.isClientAbortException(e)) {
+                    Javalin.log?.info("Exception occurred while handling static resource", e)
+                }
+            }
+        }
+        return false
+    }
+
+    private fun Resource?.isFile() = this != null && this.exists() && !this.isDirectory
+
+    private fun Resource?.isDirectoryWithWelcomeFile(handler: ResourceHandler, target: String) =
+            this != null && this.isDirectory && handler.getResource("${target.removeSuffix("/")}/index.html")?.exists() == true
+}
+
+private class WebjarHandler : ResourceHandler() {
+    override fun getResource(path: String) = Resource.newClassPathResource("META-INF/resources$path") ?: super.getResource(path)
+}
+
+private open class PrefixableHandler(private val config: StaticFileConfig) : ResourceHandler() {
+
+    init {
+        resourceBase = getResourceBase(config)
+        isDirAllowed = false
+        isEtags = true
+        Javalin.log?.info("""Static file handler added:
+        |    {urlPathPrefix: "${config.urlPathPrefix}", path: "${config.path}", location: Location.${config.location}}
+        |    Resolved path: '${getResourceBase(config)}'
+        """.trimMargin())
+    }
+
+    private fun getResourceBase(config: StaticFileConfig): String {
+        val noSuchDirMessage = "Static resource directory with path: '${config.path}' does not exist."
+        val classpathHint = "Depending on your setup, empty folders might not get copied to classpath."
+        if (config.location == Location.CLASSPATH) {
+            return Resource.newClassPathResource(config.path)?.toString() ?: throw RuntimeException("$noSuchDirMessage $classpathHint")
+        }
+        if (!File(config.path).exists()) {
+            throw RuntimeException(noSuchDirMessage)
+        }
+        return config.path
+    }
+
+    override fun getResource(path: String): Resource {
+        val targetResource by lazy { path.removePrefix(config.urlPathPrefix) }
+        return when {
+            config.urlPathPrefix == "/" -> super.getResource(path)!! // same as regular ResourceHandler
+            targetResource == "" -> super.getResource("/")!! // directory without trailing '/'
+            !path.startsWith(config.urlPathPrefix) -> EmptyResource.INSTANCE
+            !targetResource.startsWith("/") -> EmptyResource.INSTANCE
+            else -> super.getResource(targetResource)!!
+        }
+    }
+
+}
+
+private class AliasHandler(config: StaticFileConfig, private val aliasCheck: AliasCheck) : PrefixableHandler(config) {
+    override fun getResource(path: String): Resource {  // if this method throws, we get a 404
+        val resource = baseResource?.addPath(URIUtil.canonicalPath(path))!!
+        if (!resource.isAlias) return super.getResource(path) // treat as prefixablehandler
+        if (!aliasCheck.check(path, resource)) throw AccessDeniedException("Failed alias check")
+        return resource // passed check, return the resource
+    }
+}
